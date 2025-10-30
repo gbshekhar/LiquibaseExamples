5274c4c72b07f0e2c45f62dd0508143ce642ae5f
Bheemshekhar <bheemshekhar.golla@gmail.com>
2025-10-30 11:12:16 +0530

    initial code from spring.io

---
+++ LiquibaseLearningsDemo/src/main/java/com/db/liquibase/DatabaseLiquibaseDemoApplication.java
@@ -0,0 +1,13 @@
+package com.db.liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseLiquibaseDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseLiquibaseDemoApplication.class, args);
+	}
+
+}
---
+++ LiquibaseLearningsDemo/src/test/java/com/db/liquibase/DatabaseLiquibaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.liquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseLiquibaseDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
